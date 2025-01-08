8b89a8d844a27ec12ff6dbc8ceea6b75b3dbc740
Zeddic-SMO <samuel93ortil@gmail.com>
2025-01-08 02:03:27 +0100

    Reversed Interger

---
+++ 004_Integer_Reverse.java
@@ -0,0 +1,32 @@
+/*
+ * Write a function to reverse an integer using numeric operators and without using any arrays or other data structures.
+ * The signature of the function is:
+ * int f(int n)
+ */
+
+class Integer_Reverse {
+    public static void main(String[] args) {
+        System.out.println(f(12345));
+    }
+
+    static int f(int n) {
+        if (n == 0)
+            return n;
+
+        int sign = 1;
+        if (n < 0) {
+            sign = -1;
+            n = n * -1;
+        }
+
+        int reversed = 0;
+        while (n != 0) {
+            reversed = (reversed * 10) + (n % 10);
+            n = n / 10;
+        }
+
+        return sign * reversed;
+
+    }
+
+}
