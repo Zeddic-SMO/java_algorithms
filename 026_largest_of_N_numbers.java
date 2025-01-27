aaf394840738866cd6d0b9fdbd5d3fefdd7bd888
Zeddic-SMO <samuel93ortil@gmail.com>
2025-01-27 22:34:42 +0100

    Largest of N Numbers

---
+++ 026_largest_of_N_numbers.java
@@ -0,0 +1,32 @@
+// Given an integer N i.e the size of the input, followed by N more integers, find the largest of these N numbers
+// Sample Input
+// 5
+// 10 -3 2 12 7
+
+// sample output
+// 12
+import java.util.Scanner;
+
+class largets_of_N_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(largestOfN(n));
+        sc.close();
+
+    }
+
+    public static int largestOfN(int size) {
+        Scanner scanner = new Scanner(System.in);
+        int maxNum = 0;
+        for (int i = 0; i < size; i++) {
+            int num = scanner.nextInt();
+            if (num > maxNum) {
+                maxNum = num;
+            }
+        }
+        scanner.close();
+        return maxNum;
+    }
+
+}
