76d38c157d98574f94bba04a2b96391cc0ac0ab4
Zeddic-SMO <samuel93ortil@gmail.com>
2025-02-09 21:58:08 +0100

    max and Min element

--- 035_Max_Min_Element_Array.java
+++ 035_Max_Min_Element_Array.java
@@ -1,5 +1,21 @@
 // Given an array, find the maximum and the mininmum elements in it...
 
 class Max_Min_Element_Array {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int max = array[0];
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("Maximum element is " + max);
+        System.out.println("Minimum element is " + min);
+
+    }
 
 }
